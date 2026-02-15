bffcdbdcc0c3bfe0acad8cedfec014d102d1b2df
premium00495 <premium00495@gmail.com>
2026-02-15 22:51:31 +0500

    Rename and update channel loading logic
    Refactor channel loading function and update channel display.

--- app.js
+++ app.js
@@ -1,44 +1,26 @@
-let channelData = [];
-
-async function loadChannels() {
+async function loadPremiumChannels() {
   const res = await fetch("channels.json");
-  channelData = await res.json();
+  const data = await res.json();
 
   const container = document.getElementById("channels");
 
-  channelData.forEach(ch => {
+  data.forEach(ch => {
     const div = document.createElement("div");
-    div.className = "channel";
+    div.className = "channel-card";
+
     div.innerHTML = `
       <img src="${ch.thumbnail}">
-      <p>${ch.name}</p>
+      <div class="channel-info">
+        <div class="channel-name">${ch.name}</div>
+        <div class="live-tag">LIVE</div>
+      </div>
     `;
+
     div.onclick = () => {
       localStorage.setItem("channel", JSON.stringify(ch));
       window.location = "player.html";
     };
+
     container.appendChild(div);
   });
-}
-
-function loadPlayer() {
-  const ch = JSON.parse(localStorage.getItem("channel"));
-  document.getElementById("channelName").innerText = ch.name;
-
-  const video = document.getElementById("video");
-  const qualityDiv = document.getElementById("qualities");
-
-  Object.keys(ch.streams).forEach(q => {
-    const btn = document.createElement("button");
-    btn.innerText = q;
-    btn.onclick = () => {
-      video.src = ch.streams[q];
-      video.play();
-    };
-    qualityDiv.appendChild(btn);
-  });
-
-  // autoplay first quality
-  const first = Object.keys(ch.streams)[0];
-  video.src = ch.streams[first];
 }
