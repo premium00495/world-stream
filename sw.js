ab135025c6ba1e9d1c34635e634b375d5f6e549a
premium00495 <premium00495@gmail.com>
2026-02-16 16:02:46 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,28 @@
+const CACHE_NAME = "worldstream-v1";
+
+const urlsToCache = [
+  "index.html",
+  "style.css",
+  "app.js",
+  "player.html",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+// Install
 self.addEventListener("install", event => {
-  console.log("Service Worker Installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("activate", event => {
-  console.log("Service Worker Activated");
-});
-
+// Fetch
 self.addEventListener("fetch", event => {
-  // simple pass-through
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
